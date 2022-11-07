ec5bc2ab2587a1db3a6336c174e56c55e4c29053
therealvio <41095688+therealvio@users.noreply.github.com>
2022-11-08 00:20:06 +1100

    feat: Adds API key to API Gateway
    In addition to the above, the resource Ids across the stack have been
    correctly named to match the working title.
    
    This change puts down the foundations to support API Key authentication
    against the API Gateway. Though for now, the API Key requirement is not
    enabled while on client-side work is in progress.

--- backend/ops/src/index.ts
+++ backend/ops/src/index.ts
@@ -6,7 +6,7 @@ import { BackendStack } from "./stacks/backend-stack"
 const { AWS_ACCOUNT_ID: accountId, AWS_REGION: region } = process.env
 
 const app = new cdk.App()
-new BackendStack(app, "BackendStack", {
+new BackendStack(app, "legalBrawlBackendStack", {
   env: {
     account: accountId,
     region: region,
--- backend/ops/src/stacks/backend-stack.ts
+++ backend/ops/src/stacks/backend-stack.ts
@@ -4,6 +4,9 @@ import { Construct } from "constructs"
 import { aws_dynamodb as dynamodb } from "aws-cdk-lib"
 import { aws_apigateway as apigateway } from "aws-cdk-lib"
 import { aws_lambda as lambda } from "aws-cdk-lib"
+import { aws_iam as iam } from "aws-cdk-lib"
+import { aws_secretsmanager as secretsmanager } from "aws-cdk-lib"
+import { aws_ssm as ssm } from "aws-cdk-lib"
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -22,30 +25,78 @@ export class BackendStack extends cdk.Stack {
 
     //The Lambda to handle requests coming through the API Gateway
     //Lambda would be written in golang
-    const lambdafn = new lambda.Function(this, "legalBattleApiHandler", {
+    const lambdafn = new lambda.Function(this, "legalBrawlApiHandler", {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.PROVIDED_AL2,
       handler: "bootstrap",
       code: lambda.Code.fromAsset(path.join(__dirname, "../../../src/dist")),
+      environment: {
+        LEGAL_BRAWL_SECRET_NAME: "legalBrawl/prod/api/v1/",
+      },
     })
 
-    const api = new apigateway.LambdaRestApi(this, "legalBattleApi", {
+    //TODO: Swap this out so the ARN suffix isn't stated in source code
+    const apiToken = secretsmanager.Secret.fromSecretNameV2(
+      this,
+      "apiTokenSecret",
+      "legalBrawl/prod/api/v1/"
+    )
+    const SecretArnSuffix = ssm.StringParameter.valueForStringParameter(
+      this,
+      "/legalBrawl/secret/arn/suffix"
+    )
+    lambdafn.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+        ],
+        resources: [`${apiToken.secretArn}-${SecretArnSuffix}`],
+      })
+    )
+
+    const api = new apigateway.LambdaRestApi(this, "legalBrawlApi", {
       handler: lambdafn,
       //Setting proxy to `true` will forward *all* requests to Lambda
       //TODO: Figure out what resources, and what methods we are looking to implement
       proxy: false,
     })
 
+    new ssm.StringParameter(this, "paramLegalBrawlApiUrl", {
+      parameterName: "/legalBrawl/api/url",
+      stringValue: api.url,
+    })
+
     //b.s. example of API paths
     const v1 = api.root.addResource("v1")
     const formation = v1.addResource("formationId")
-    formation.addMethod("POST")
+    const formationPostMethod = formation.addMethod("POST", undefined, {
+      apiKeyRequired: false,
+    }) //Set apiKeyRequired to `true` when ready to start locking down the API
 
-    const plan = api.addUsagePlan("legalBattleUsagePlan", {
+    const plan = api.addUsagePlan("legalBrawlUsagePlan", {
       throttle: {
         rateLimit: 10,
         burstLimit: 2,
       },
     })
+
+    plan.addApiStage({
+      stage: api.deploymentStage,
+      throttle: [
+        {
+          method: formationPostMethod,
+          throttle: {
+            rateLimit: 10,
+            burstLimit: 2,
+          },
+        },
+      ],
+    })
+
+    const key = api.addApiKey("ApiKey", {
+      apiKeyName: "ApiKey",
+    })
+    plan.addApiKey(key)
   }
 }
