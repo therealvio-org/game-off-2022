117dce488e011de2976bddf1ba8447be22aa0991
therealvio <41095688+therealvio@users.noreply.github.com>
2022-11-12 19:38:13 +1100

    feat: Adds formal API path for playerHand The `playerHand` path has been added to the legalBrawl API. This will be the destination where POST requests are made by the game client to submit, and in return receive an opposing player's selected cards.
    A model has been added to ensure that the body's JSON data is valid
    before it reaches the Lambda (though this doesn't stop unsanitary
    inputs, it's a matter of validation at a schema-level).

--- backend/ops/src/stacks/backend-stack.ts
+++ backend/ops/src/stacks/backend-stack.ts
@@ -1,10 +1,10 @@
 import * as cdk from "aws-cdk-lib"
 import * as path from "path"
 import { Construct } from "constructs"
-import { aws_dynamodb as dynamodb } from "aws-cdk-lib"
 import { aws_apigateway as apigateway } from "aws-cdk-lib"
-import { aws_lambda as lambda } from "aws-cdk-lib"
+import { aws_dynamodb as dynamodb } from "aws-cdk-lib"
 import { aws_iam as iam } from "aws-cdk-lib"
+import { aws_lambda as lambda } from "aws-cdk-lib"
 import { aws_secretsmanager as secretsmanager } from "aws-cdk-lib"
 import { aws_ssm as ssm } from "aws-cdk-lib"
 
@@ -12,26 +12,25 @@ export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    //Best Practices:
-    //https://docs.aws.amazon.com/prescriptive-guidance/latest/patterns/estimate-the-cost-of-a-dynamodb-table-for-on-demand-capacity.html?did=pg_card&trk=pg_card#estimate-the-cost-of-a-dynamodb-table-for-on-demand-capacity-best-practices
-    new dynamodb.Table(this, "gameDatabase", {
-      partitionKey: { name: "playerId", type: dynamodb.AttributeType.STRING },
-      sortKey: { name: "formationId", type: dynamodb.AttributeType.STRING },
+    const playerHandsTable = new dynamodb.Table(this, "playerHandsTable", {
+      partitionKey: { name: "version", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "playerId", type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PROVISIONED,
       pointInTimeRecovery: false,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      tableName: "gameDatabase",
+      removalPolicy: cdk.RemovalPolicy.DESTROY, //Swap this to `retain` when we got something that is shaping up nicely
+      tableName: "playerHands",
     })
 
     //The Lambda to handle requests coming through the API Gateway
     //Lambda would be written in golang
-    const lambdafn = new lambda.Function(this, "legalBrawlApiHandler", {
+    const lambdaFn = new lambda.Function(this, "legalBrawlApiHandler", {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.PROVIDED_AL2,
       handler: "bootstrap",
       code: lambda.Code.fromAsset(path.join(__dirname, "../../../src/dist")),
       environment: {
         LEGAL_BRAWL_SECRET_NAME: "legalBrawl/prod/api/v1/",
+        PLAYER_HAND_TABLE_NAME: playerHandsTable.tableName,
       },
     })
 
@@ -45,21 +44,53 @@ export class BackendStack extends cdk.Stack {
       this,
       "/legalBrawl/secret/arn/suffix"
     )
-    lambdafn.addToRolePolicy(
+    lambdaFn.addToRolePolicy(
       new iam.PolicyStatement({
         actions: [
           "secretsmanager:GetSecretValue",
           "secretsmanager:DescribeSecret",
+          "dynamodb:PutItem",
         ],
-        resources: [`${apiToken.secretArn}-${SecretArnSuffix}`],
+        resources: [
+          `${apiToken.secretArn}-${SecretArnSuffix}`,
+          playerHandsTable.tableArn,
+        ],
       })
     )
 
     const api = new apigateway.LambdaRestApi(this, "legalBrawlApi", {
-      handler: lambdafn,
+      handler: lambdaFn,
       //Setting proxy to `true` will forward *all* requests to Lambda
       //TODO: Figure out what resources, and what methods we are looking to implement
       proxy: false,
+    })
+
+    const apiIntegration = new apigateway.LambdaIntegration(lambdaFn)
+
+    //playerHand scheme
+    const playerHandModel = api.addModel("playerHand", {
+      contentType: `application/json`,
+      modelName: "playerHandv1",
+      schema: {
+        schema: apigateway.JsonSchemaVersion.DRAFT4,
+        title: "PlayerHandPost",
+        type: apigateway.JsonSchemaType.OBJECT,
+        required: ["handInfo"],
+        properties: {
+          handInfo: {
+            type: apigateway.JsonSchemaType.OBJECT,
+            properties: {
+              playerName: { type: apigateway.JsonSchemaType.STRING },
+              playerId: { type: apigateway.JsonSchemaType.STRING },
+              version: { type: apigateway.JsonSchemaType.STRING },
+              cards: {
+                type: apigateway.JsonSchemaType.ARRAY,
+                items: { type: apigateway.JsonSchemaType.INTEGER },
+              },
+            },
+          },
+        },
+      },
     })
 
     new ssm.StringParameter(this, "paramLegalBrawlApiUrl", {
@@ -70,9 +101,21 @@ export class BackendStack extends cdk.Stack {
     //b.s. example of API paths
     const v1 = api.root.addResource("v1")
     const formation = v1.addResource("formationId")
-    const formationPostMethod = formation.addMethod("POST", undefined, {
+    const playerHand = v1.addResource("playerHand")
+    const formationPostMethod = formation.addMethod("POST", apiIntegration, {
       apiKeyRequired: false,
     }) //Set apiKeyRequired to `true` when ready to start locking down the API
+    const playerHandPost = playerHand.addMethod("POST", apiIntegration, {
+      apiKeyRequired: true,
+      requestModels: {
+        "application/json": playerHandModel,
+      },
+      requestValidatorOptions: {
+        requestValidatorName: "playerHandValidator",
+        validateRequestBody: true,
+        validateRequestParameters: false,
+      },
+    })
 
     const plan = api.addUsagePlan("legalBrawlUsagePlan", {
       throttle: {
@@ -86,6 +129,13 @@ export class BackendStack extends cdk.Stack {
       throttle: [
         {
           method: formationPostMethod,
+          throttle: {
+            rateLimit: 10,
+            burstLimit: 2,
+          },
+        },
+        {
+          method: playerHandPost,
           throttle: {
             rateLimit: 10,
             burstLimit: 2,
